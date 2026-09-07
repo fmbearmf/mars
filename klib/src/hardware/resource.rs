e312bac761df94c0e472235e460ffe4e9d3bb3b0
fmbearmf <77757734+fmbearmf@users.noreply.github.com>
2026-09-06 22:50:58 -0700

    klib: add PciEcam resource

--- klib/src/hardware/resource.rs
+++ klib/src/hardware/resource.rs
@@ -3,8 +3,19 @@ use core::ops::Range;
 /// descriptor of a hardware resource
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Resource {
-    Mmio { range: Range<usize> },
+    Mmio {
+        range: Range<usize>,
+    },
     Irq(u32),
+    PciEcam {
+        segment: u16,
+        bus: u8,
+        device: u8,
+        function: u8,
+        ecam_phys_base: u64,
+        ecam_start_bus: u8,
+        ecam_end_bus: u8,
+    },
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
